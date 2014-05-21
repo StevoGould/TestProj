3e90fe4419bb1349271b44dde96a8297fe29e349
AutoBotG <Girish.Pottigari@uk.tesco.com>
2014-05-21 11:39:34 +0100

    adding a patch  to WebDriverListiner used in Browser Mob

---
+++ features/support/tesco_browser_mob.rb
@@ -0,0 +1,40 @@
+module BrowserMob
+  module Proxy
+    class Client
+      def self.from(server_url)
+        port = JSON.parse(
+            RestClient.post(URI.join(server_url, "proxy?httpProxy=dev01isa01:8080").to_s, '')
+        ).fetch('port')
+
+        uri = URI.parse(File.join(server_url, "proxy", port.to_s))
+        resource = RestClient::Resource.new(uri.to_s)
+
+        Client.new resource, uri.host, port
+      end
+    end
+    class WebDriverListener
+      def har_presnt
+        begin
+          @client.har
+        rescue JSON::ParserError => e
+          false
+        else
+          true
+        end
+      end
+
+      def before_navigate_to(url, driver)
+        save_har if har_presnt # first request
+        @client.new_har("navigate-to-#{url}", @new_har_opts)
+        @current_url = url
+      end
+
+      def before_quit(driver)
+        #save_har
+      end
+    end
+
+  end
+
+end
+
